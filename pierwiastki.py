71b166a6e809ef545eab2b2760a0f624349354f2
Admmich <admmichkox@gmail.com>
2023-11-17 10:17:02 +0100

    pierwiastkix

--- pierwiastki.py
+++ pierwiastki.py
@@ -7,14 +7,15 @@ import math
 # print(delta)
 
 #napisz program podajacy winik obliczenia zamiany ulamka binarnego na liczby dziesietne przezanianie podajemy ilosc bitow na jakich jest zapisana podana liczxba
-def binToFloat(n):
+def binToFloat(n, ileBit=0):
     wynik = 0
-    for i in range(2, len(n)):
-        if n[i] == '1' and n[i] == 0:
+    for i in range(2, ileBit+2):
+        if n[i] == '1' or n[i] == '0':
             if n[i] == '1':
                 wynik += 2 ** (-i+1)
         else:
             return  "ERROR"
     return wynik
 
-print(binToFloat("0,1"))
+print(binToFloat("0,000110011001100110011001100110011", 4))
+
