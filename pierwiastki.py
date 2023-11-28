92cccb925167058063dc01fb83879d1a7f8fc064
Admmich <admmichkox@gmail.com>
2023-11-28 14:56:41 +0100

    28

--- pierwiastki.py
+++ pierwiastki.py
@@ -1,3 +1,102 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from random import choices
+
+
+def los(n):
+    return choices((1, 2, 3, 4, 5, 6), k=n)
+
+
+def rzut(pkt, rzut_kostka):
+    if rzut_kostka == 6:
+        return pkt
+    if rzut_kostka in (5, 4):
+        return pkt * 2
+    if rzut_kostka == 3 or rzut_kostka == 2:
+        return pkt // 2
+    if rzut_kostka == 1:
+        return 1
+
+
+def rezultat(pkt, tab):
+    for i in tab:
+        pkt = rzut(pkt, i)
+    return pkt
+
+
+tab = los(3)
+print(tab)
+print(rezultat(100, tab))
+
 import math
 # a=0.10000
 # b=150
@@ -82,30 +181,6 @@ import math
 # print(y)
 
 
-from random import choices
-
-def los(n):
-    return choices((1,2,3,4,5,6), k=n)
-
-def rzut(pkt, rzut_kostka):
-    if rzut_kostka == 6:
-        return pkt
-    if rzut_kostka in (5,4):
-        return pkt * 2
-    if rzut_kostka == 3 or rzut_kostka == 2:
-        return  pkt //2
-    if rzut_kostka == 1:
-        return 1
 
 
-def rezultat(pkt, tab):
-    for i in tab:
-        pkt = rzut(pkt, i)
-    return pkt
 
-tab = los(3)
-print(tab)
-print(rezultat(100,tab))
-
-
-haloo
